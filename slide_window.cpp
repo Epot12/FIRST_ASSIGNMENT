9174e5a4c2bb4015c5db9c1779f621da15ca8ffe
Emi <e.lorenzo0203@gmail.com>
2026-04-02 18:34:32 +0200

    Added function slide_window

--- slide_window.cpp
+++ slide_window.cpp
@@ -2,13 +2,15 @@
 // Created by UTENTE on 02/04/2026.
 //
 
+#include <valarray>
 #include "slide_window.h"
 
-float slide_window(vector<real_t> query, vector<real_t> window) {
+real_t slide_window(vector<real_t> query, vector<real_t> window) {
     real_t sad;
     size_t length;
-    length = query.size();
+    length = window.size();
     for (int i = 0; i < length; i ++) {
-
+        sad += abs(window[i] - query[i]);
     }
+    return sad;
 }
--- slide_window.h
+++ slide_window.h
@@ -11,13 +11,6 @@ using real_t = double;
 using namespace std;
 
 
-float slide_window(vector<real_t> query, vector<real_t> window) {
-    real_t sad;
-    size_t length;
-    length = query.size();
-    for (int i = 0; i < length; i ++) {
-
-    }
-}
+real_t slide_window(vector<real_t> query, vector<real_t> window);
 
 #endif //FIRST_ASSIGNMENT_SLIDE_WINDOW_H
