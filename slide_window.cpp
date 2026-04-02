8132feea08fcf99e613bece27ae9385f2a87fa5f
Emi <e.lorenzo0203@gmail.com>
2026-04-02 18:26:41 +0200

    Writing c logic for sequential program

---
+++ slide_window.cpp
@@ -0,0 +1,14 @@
+//
+// Created by UTENTE on 02/04/2026.
+//
+
+#include "slide_window.h"
+
+float slide_window(vector<real_t> query, vector<real_t> window) {
+    real_t sad;
+    size_t length;
+    length = query.size();
+    for (int i = 0; i < length; i ++) {
+
+    }
+}
---
+++ slide_window.h
@@ -0,0 +1,23 @@
+//
+// Created by UTENTE on 02/04/2026.
+//
+
+#ifndef FIRST_ASSIGNMENT_SLIDE_WINDOW_H
+#define FIRST_ASSIGNMENT_SLIDE_WINDOW_H
+
+#include <vector>
+
+using real_t = double;
+using namespace std;
+
+
+float slide_window(vector<real_t> query, vector<real_t> window) {
+    real_t sad;
+    size_t length;
+    length = query.size();
+    for (int i = 0; i < length; i ++) {
+
+    }
+}
+
+#endif //FIRST_ASSIGNMENT_SLIDE_WINDOW_H
