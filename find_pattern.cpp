cd569034671bc8289554469f82fc9b4e873a2f67
Emi <e.lorenzo0203@gmail.com>
2026-04-03 10:23:23 +0200

    Backup commit: find_pattern logic has been written. Now it needs to be checked.

--- find_pattern.cpp
+++ find_pattern.cpp
@@ -4,7 +4,7 @@
 match_result find_pattern(vector<real_t> query, vector<vector<real_t>> &data) {
     int db_size = data.size();
     int query_size = query.size();
-    real_t best_distance = numeric_limits<real_t>::infinity();
+    match_result best_result;
     for (int i = 0; i < db_size; i ++) {
         int series_size = data[i].size();
         size_t series_id = data[i][0];
@@ -13,10 +13,11 @@ match_result find_pattern(vector<real_t> query, vector<vector<real_t>> &data) {
             for (int k = 0; k < query_size; k ++){
                 distance += abs(query[i] - data[i][j + k]);
             }
-            if (distance < best_distance) {
-                best_distance = distance;
+            if (distance < best_result.distance) {
+                best_result = match_result(series_id, j, distance);
             }
 
         }
     }
+    return best_result;
 }
