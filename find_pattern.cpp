e363d3fc3f2db1b8f6d5e2c81f91733105af0159
Emi <e.lorenzo0203@gmail.com>
2026-04-03 09:58:49 +0200

    Backup commit before heavy changes on slide_window code

--- find_pattern.cpp
+++ find_pattern.cpp
@@ -1,5 +1,13 @@
-//
-// Created by UTENTE on 02/04/2026.
-//
-
 #include "find_pattern.h"
+
+match_result find_pattern(vector<real_t> query, vector<vector<real_t>> data) {
+    int db_size = data.size();
+    match_result result = match_result();
+    for (int i = 0; i < db_size; i ++) {
+        int series_size = data[i].size();
+        for (int j = 0; j < series_size; j ++){
+            vector<real_t> window =
+            real_t distance = slide_window(query, )
+        }
+    }
+}
--- find_pattern.h
+++ find_pattern.h
@@ -9,14 +9,6 @@
 #include "match_result.h"
 using namespace std;
 
-match_result find_pattern(vector<real_t> query, vector<vector<real_t>> data) {
-    int db_size = data.size();
-    for (int i = 0; i < db_size; i ++) {
-        int series_size = data[i].size();
-        for (int j = 0; j < series_size; j ++){
-
-        }
-    }
-}
+match_result find_pattern(vector<real_t> query, vector<vector<real_t>> data) {}
 
 #endif //FIRST_ASSIGNMENT_FIND_PATTERN_H
