eb5a7e7937ef993e7bfd98778c067c7ea7ac73e5
Emi <e.lorenzo0203@gmail.com>
2026-04-03 10:43:21 +0200

    Backup commit: the implementation of the core sequential program is now concluded. Mut be evaluated whether normalize or not the UCR data.

--- find_pattern.cpp
+++ find_pattern.cpp
@@ -1,22 +1,25 @@
-#include <valarray>
+#include <cmath>
 #include "find_pattern.h"
 
-match_result find_pattern(vector<real_t> query, vector<vector<real_t>> &data) {
-    int db_size = data.size();
-    int query_size = query.size();
-    match_result best_result;
-    for (int i = 0; i < db_size; i ++) {
-        int series_size = data[i].size();
-        size_t series_id = data[i][0];
-        for (int j = 0; j < series_size - query_size; j ++){
-            real_t distance = 0.0;
-            for (int k = 0; k < query_size; k ++){
-                distance += abs(query[i] - data[i][j + k]);
-            }
-            if (distance < best_result.distance) {
-                best_result = match_result(series_id, j, distance);
-            }
+using namespace std;
 
+match_result find_pattern(const vector<real_t> &query, const vector<vector<real_t>> &data) {
+    size_t db_size = data.size();
+    size_t query_size = query.size();
+    match_result best_result;
+    for (size_t i = 0; i < db_size; i ++) {
+        size_t series_size = data[i].size();
+        if (series_size > query_size) {
+            for (size_t j = 1; j <= series_size - query_size; j++) {
+                real_t distance = 0.0;
+                for (size_t k = 0; k < query_size; k++) {
+                    distance += abs(query[k] - data[i][j + k]);
+                }
+                if (distance < best_result.distance) {
+                    best_result = match_result(i, j, distance);
+                }
+
+            }
         }
     }
     return best_result;
--- find_pattern.h
+++ find_pattern.h
@@ -1,14 +1,10 @@
-//
-// Created by UTENTE on 02/04/2026.
-//
 
 #ifndef FIRST_ASSIGNMENT_FIND_PATTERN_H
 #define FIRST_ASSIGNMENT_FIND_PATTERN_H
 
 #include <vector>
 #include "match_result.h"
-using namespace std;
 
-match_result find_pattern(vector<real_t> query, vector<vector<real_t>> &data) {}
+match_result find_pattern(const std::vector<real_t> &query, const std::vector<std::vector<real_t>> &data);
 
 #endif //FIRST_ASSIGNMENT_FIND_PATTERN_H
