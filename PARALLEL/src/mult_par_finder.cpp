e2fd71af78c62a65c992ce587fc7b94c02603d04
Epot12 <e.lorenzo0203@gmail.com>
2026-04-06 16:37:41 +0200

    Created new function mult_par_finder to implement parallelism at pattern search level

---
+++ PARALLEL/src/mult_par_finder.cpp
@@ -0,0 +1,9 @@
+
+#include "mult_par_finder.h"
+
+
+std::vector<match_result> mult_par_finder(
+        const std::vector<real_t> &flat_queries, size_t query_length,
+        const std::vector<real_t> &flat_data, const std::vector<size_t>& data_offsets) {
+
+}
---
+++ PARALLEL/src/mult_par_finder.h
@@ -0,0 +1,12 @@
+
+#ifndef FIRST_ASSIGNMENT_MULT_PAR_FINDER_H
+#define FIRST_ASSIGNMENT_MULT_PAR_FINDER_H
+
+#include <vector>
+#include "../../SEQUENTIAL/headers/match_result.h"
+
+std::vector<match_result> mult_par_finder(
+        const std::vector<real_t> &flat_queries, size_t query_length,
+        const std::vector<real_t> &flat_data, const std::vector<size_t>& data_offsets);
+
+#endif //FIRST_ASSIGNMENT_MULT_PAR_FINDER_H
