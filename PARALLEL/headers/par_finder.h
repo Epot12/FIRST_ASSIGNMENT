1f37a630c35a560f00190e0df92d157da03ad637
Epot12 <e.lorenzo0203@gmail.com>
2026-04-18 12:45:17 +0200

    Modified the guards on the top of parallelized files

--- PARALLEL/headers/par_finder.h
+++ PARALLEL/headers/par_finder.h
@@ -1,6 +1,6 @@
 
-#ifndef FIRST_ASSIGNMENT_FIND_PATTERN_H
-#define FIRST_ASSIGNMENT_FIND_PATTERN_H
+#ifndef FIRST_ASSIGNMENT_PAR_FINDER_H
+#define FIRST_ASSIGNMENT_PAR_FINDER_H
 
 #include <vector>
 #include "../../SEQUENTIAL/headers/match_result.h"
@@ -9,4 +9,4 @@ std::vector<match_result> par_finder(
         const std::vector<real_t> &flat_queries, size_t query_length,
         const std::vector<real_t> &flat_data, const std::vector<size_t>& data_offsets);
 
-#endif //FIRST_ASSIGNMENT_FIND_PATTERN_H
+#endif //FIRST_ASSIGNMENT_PAR_FINDER_H
